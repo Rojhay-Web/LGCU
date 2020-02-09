7193b0c0c8be0cbb805c7ee34add95b6ea3973d4
reddingk <kris.redding3@gmail.com>
2020-02-09 03:24:15 -0500

    update

--- src/js/app.js
+++ src/js/app.js
@@ -23,7 +23,7 @@ import TranslateVideo from './templates/components/translateVideoModal';
 import "../css/app.less";
 
 /* Images */
-import logo from '../assets/LGCULogo2_full.png';
+import logo from '../assets/LGCULogo2_full.PNG';
 import logotxt from '../assets/LGCULogo2_txt.png';
 
 const history = createBrowserHistory(); 
