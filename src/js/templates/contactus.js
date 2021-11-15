bf5ca5e3362c53f28649d1d6489fb1add05c2f1c
reddingk <kris.redding3@gmail.com>
2021-11-15 13:00:36 -0500

    update contact us page and footer

--- src/js/templates/contactus.js
+++ src/js/templates/contactus.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import ReactGA from 'react-ga';
 
+import SbEditable from 'storyblok-react';
+import StoryblokService from '../utils/storyblok.service';
+
 /* Images */
 import back1 from '../../assets/site/mini/back12.jpg';
 import emailImg from '../../assets/site/mini/email.jpg';
 
 /* Components */
 import FormCpt from './components/formCpt';
+
+const stb = new StoryblokService();
 
 /* Header */
 class ContactHeader extends Component{
@@ -31,6 +36,7 @@ class Contact extends Component{
     constructor(props) {
         super(props);
         this.state = {
+            contactCards:[],
             contactForm:{
                 "title":"contact us", "sendAddress":"admissions@lenkesongcu.org",
                 "subject":"Contact Us Form Submission", "additionalData":{}, "type":"core",
@@ -43,16 +49,38 @@ class Contact extends Component{
             }
         }
         this.initialReactGA = this.initialReactGA.bind(this);
+        this.loadPageData = this.loadPageData.bind(this);
     }
 
     componentDidMount(){ 
         window.scrollTo(0, 0);
         this.initialReactGA();
+        this.loadPageData();
     }
 
     initialReactGA(){
         ReactGA.initialize('UA-147138083-1');
         ReactGA.pageview('/contactUs');
+    }
+
+    loadPageData(){
+        var self = this;
+        try {
+            stb.initEditor(this);
+            stb.getInitialProps({"query":"contactus"}, 'cdn/stories/contactus', function(page){
+                if(page){
+                    var body = page.data.story.content.contactList;
+                    
+                    if(body) { 
+                        var tmpBody = body.filter(function(item){ return item.component.toLowerCase() === "contactcard"; });                       
+                        self.setState({ contactCards: tmpBody });
+                    }
+                }
+            });
+        }
+        catch(ex){
+            console.log("Error Loading Page Data: ",ex);
+        }
     }
 
     render(){        
@@ -61,7 +89,23 @@ class Contact extends Component{
                 <section className="contactus-section contact-form">
                     <div className="section-container">                        
                         <div className="split-info">
-                            <div className="split-img"><img alt="Contact info img" src={emailImg} /></div>
+                            {/*<div className="split-img"><img alt="Contact info img" src={emailImg} /></div>*/}
+                            <div className="split-contacts">
+                                <h2 className="lrgTitle ctr c0" data-text="Get In Touch">Get In Touch</h2>
+                                <p>We would love to hear from you with any questions about our university and how it can best serve you.</p>
+                                <div className="card-box">
+                                    {this.state.contactCards.map((card,i) =>
+                                        <div className="card-container" key={i}>
+                                            <i className="far fa-address-card" />
+                                            <div className="info">
+                                                <div className="contact-title">{card.title}</div> 
+                                                <div className="contact-info">{card.contactText}</div>
+                                                <div className="contact-info sub">{card.subText}</div>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
                             <div className="split-txt">
                                 <h2 className="lrgTitle ctr" data-text="Contact Us">Contact Us</h2>
                                 <p>Thank you for your interest in LGCU. Complete the form to get in touch with our offices to answer any questions regarding our university.</p>
